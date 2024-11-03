a2bbc45d8fe347d6db629bba93ab446ca52a9688
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-11-03 15:24:16 -0500

    [init] design-sys-cli-render

--- build.js
+++ build.js
@@ -1,13 +1,13 @@
 import { merger, build_SASS, cssMerger } from "merger-client-static-jsx";
 
-await build_SASS({
-  mainSASS: "./DocuDesign/scss/abrevs.scss",
-  outCSS: "./DocuDesign/css/main-sass.css",
-});
+// await build_SASS({
+//   mainSASS: "./DocuDesign/scss/abrevs.scss",
+//   outCSS: "./DocuDesign/css/main-sass.css",
+// });
 
-await cssMerger({ folderCSS: "./DocuDesign/css", outfile: "./DocuDesign/_static/all.styles.min.css" })
+await cssMerger({ folderCSS: "./DocuDesign/css", outfile: "./DocuDesign/static/all.styles.min.css" })
 
-merger({
-  folderRoot: "./DocuDesign",
-  output: "./DocuDesign/_static/all.templates.min.js",
-});
+// merger({
+//   folderRoot: "./DocuDesign",
+//   output: "./DocuDesign/static/all.templates.min.js",
+// });
