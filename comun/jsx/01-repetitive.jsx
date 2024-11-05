70cfc97bedb607b69a2a08eb28e33e255a3b29b9
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-11-04 21:04:53 -0500

    [mod] nuevos componentes repetitivos

--- comun/jsx/01-repetitive.jsx
+++ comun/jsx/01-repetitive.jsx
@@ -1,10 +1,21 @@
+function $enfasis({ children }) {
+  return <strong className="c-skyblue">{children}</strong>;
+}
+
+function $secundario({ children }) {
+  return <span style={{ color: "steelblue" }}>{children}</span>;
+}
+
 function $CardDef(props) {
   const { title, children } = props;
   delete props.title;
   delete props.children;
+  if (typeof title == "string") {
+    title = <$enfasis>{title}</$enfasis>;
+  }
   return (
     <Card {...props} className="mh-20px pad-10px">
-      <$enfasis>{title}</$enfasis>
+      {title}
       <br />
       <$F>{children}</$F>
     </Card>
