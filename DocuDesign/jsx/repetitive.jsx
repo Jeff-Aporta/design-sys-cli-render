9fb265960401fb3004a760485fce25468274b83a
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-11-04 21:11:19 -0500

    [mod] reorder

--- DocuDesign/jsx/repetitive.jsx
+++ DocuDesign/jsx/repetitive.jsx
@@ -1,5 +1,40 @@
+
+function $CardDef(props) {
+  let { title, children } = props;
+  delete props.title;
+  delete props.children;
+  if (typeof title == "string") {
+    title = <$enfasis>{title}</$enfasis>;
+  }
+  return (
+    <Card {...props} className="mh-20px pad-10px">
+      {title}
+      <br />
+      <$F>{children}</$F>
+    </Card>
+  );
+}
+
+function $CardF(props) {
+  return (
+    <Card
+      {...props}
+      className={`${props.className ?? ""} pad-10px`}
+      children={<$F>{props.children}</$F>}
+    />
+  );
+}
+
 function $index(props) {
   return (
     <$ {...props} id={idR()} className={`${props.className} indexed md`} />
   );
+}
+
+function $enfasis({ children }) {
+  return <strong className="c-skyblue">{children}</strong>;
+}
+
+function $secundario({ children }) {
+  return <span style={{ color: "steelblue" }}>{children}</span>;
 }
--- comun/jsx/01-repetitive.jsx
+++ comun/jsx/01-repetitive.jsx
@@ -1,27 +1,3 @@
-function $enfasis({ children }) {
-  return <strong className="c-skyblue">{children}</strong>;
-}
-
-function $secundario({ children }) {
-  return <span style={{ color: "steelblue" }}>{children}</span>;
-}
-
-function $CardDef(props) {
-  const { title, children } = props;
-  delete props.title;
-  delete props.children;
-  if (typeof title == "string") {
-    title = <$enfasis>{title}</$enfasis>;
-  }
-  return (
-    <Card {...props} className="mh-20px pad-10px">
-      {title}
-      <br />
-      <$F>{children}</$F>
-    </Card>
-  );
-}
-
 function idR(prefix = "idR") {
   return Math.random()
     .toString(36)
@@ -47,16 +23,6 @@ function $CardCopy(props) {
     <$Copy>
       <Card {...props} className={`${props.className ?? ""} pad-10px`} />
     </$Copy>
-  );
-}
-
-function $CardF(props) {
-  return (
-    <Card
-      {...props}
-      className={`${props.className ?? ""} pad-10px`}
-      children={<$F>{props.children}</$F>}
-    />
   );
 }
 
