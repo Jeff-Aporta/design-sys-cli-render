d7fabcb367b2da1e4c1b47f0ef0d6fb76314ec71
jeff-aporta <jeffaporta@gmail.com>
2025-02-26 21:47:14 -0500

    [feat] mejorar el editor en línea: agregar consola de depuración para la clase de estilo en HeaderMenu

--- DocuDesign/jsx/HeaderMenu.jsx
+++ DocuDesign/jsx/HeaderMenu.jsx
@@ -131,6 +131,7 @@ function IconSocialTop({ lbl, url, _icon, black = false, color, brk = 700 }) {
   );
   const alw = "d-center gap-10px c-skyblue bright-hover-1-5";
   const cls = fluidCSS().ltX(brk, { display: "none" }).end(alw);
+  console.log(cls)
   const full = (
     <span className={cls}>
       {icon}
