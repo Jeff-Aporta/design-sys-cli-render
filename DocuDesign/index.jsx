f85c84c5c1cef0f08227ea0d800f66ac71063ae9
jeff-aporta <jeffaporta@gmail.com>
2025-03-04 23:57:14 -0500

    [feat] mejorar el editor en línea: corregir formato y agregar actualización de estilos con fluidCSS

--- DocuDesign/index.jsx
+++ DocuDesign/index.jsx
@@ -1,13 +1,14 @@
 function App() {
-    return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Paper className="pad-10px m-30px">
-          <h1>En Contstrucción</h1>
-        </Paper>
-      </ThemeProvider>
-    );
-  }
-  
-  ReactDOM.render(<App />, document.getElementById("root"));
-  
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Paper className="pad-10px m-30px">
+        <h1>En Contstrucción</h1>
+      </Paper>
+    </ThemeProvider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+setTimeout(fluidCSS().actualizarStyle);
